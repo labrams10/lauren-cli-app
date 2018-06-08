bf17b63700977995a087519d82b7ad50a74b4df9
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-06-07 21:07:57 -0400

    got rid of my in-line comments, cleaned everything up

--- lib/bestsellers/scraper.rb
+++ lib/bestsellers/scraper.rb
@@ -10,7 +10,7 @@ require 'pry'
   end
 
   def self.get_categories_with_books
-    get_page.css("section.subcategory").each do |category| #scrape each category with the books inside, and create the instance of category
+    get_page.css("section.subcategory").each do |category|
       category_name = Bestsellers::Category.new(category.css("a.subcategory-heading-link").text.strip)
       category.css("article.book").each do |book|
         new_book = Book.new(
@@ -24,6 +24,3 @@ require 'pry'
     end
   end
 end
-#main > div.view.landing-view > section:nth-child(1) > ol > li:nth-child(1) > article
-
-#Why does each category only have one instance of book? Each one should have 5? is it an issue with my reader or the writer within the method above?
